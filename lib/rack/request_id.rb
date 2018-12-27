e9aee7b8933b57e71e2e0c9b71337e7fa307717e
Michael Lovitt <michael@lovitt.net>
2018-12-27 09:21:02 -0600

    set request-id to env directly since other middleware expects to find it there

--- lib/rack/request_id.rb
+++ lib/rack/request_id.rb
@@ -3,6 +3,7 @@ require 'securerandom'
 module Rack
   class RequestId
 
+    ENV_KEY = 'HTTP_X_REQUEST_ID'
     HEADER_KEY = 'X-Request-Id'
 
     def initialize(app, opts = {})
@@ -15,16 +16,15 @@ module Rack
       storage = @storage.respond_to?(:call) ? @storage.call : @storage
       request = Rack::Request.new(env)
 
-      if request_id = request.get_header(HEADER_KEY)
-        storage[:request_id] = request_id
-      else
-        storage[:request_id] = @id_generator.call
-        request.set_header(HEADER_KEY, storage[:request_id])
-      end
+      request_id = env[ENV_KEY] || request.get_header(HEADER_KEY) || @id_generator.call
+
+      storage[:request_id] = request_id
+      request.set_header(HEADER_KEY, request_id)
+      env[ENV_KEY] = request_id
 
       status, headers, body = @app.call(env)
+      headers[HEADER_KEY] ||= request_id
 
-      headers[HEADER_KEY] ||= storage[:request_id]
       [status, headers, body]
     end
   end
